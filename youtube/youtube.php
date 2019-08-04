2851c4bb8a2abdfcc2e3e9b9f596d4610c781ec7
Jamsheed Mistri <jmistri7@gmail.com>
2019-08-03 18:56:34 -0700

    Fix typo in youtube.php

--- youtube/youtube.php
+++ youtube/youtube.php
@@ -2,7 +2,7 @@
 require_once '../config/config.ini.php';
 
 $search = str_replace(" ", "%20", $_GET['search']);
-$results = json_decode(file_get_contents("https://www.googleapis.com/youtube/v3/search?key=$YOUTUBE_API_KEY=1&part=snippet&q=" . $search . 
+$results = json_decode(file_get_contents("https://www.googleapis.com/youtube/v3/search?key=$YOUTUBE_API_KEY&part=snippet&q=" . $search . 
 "&type=video"), true);
 
 echo $results["items"][0]["id"]["videoId"];
